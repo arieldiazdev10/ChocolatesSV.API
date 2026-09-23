8cb1af81576fab144cf63fea2ddbe14e6919dcb0
arieldiazdev10 <ariel.ernesto.diaz.carpio@gmail.com>
2026-09-22 22:54:51 -0600

    Se implementa Identity para realizar autenticación y autorización de los endpoints del modo administrador

--- ChocolatesSV.API/Program.cs
+++ ChocolatesSV.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ChocolatesSV.Entities.Models;
+using ChocolatesSV.DAL;
 using Scalar.AspNetCore;
 using ChocolatesSV.Common;
 using ChocolatesSV.DAL.Services;
@@ -5,19 +9,33 @@ using ChocolatesSV.BL.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. Configuraciones existentes
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// 2. Conexión de DbContext para Identity
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration["AppSettings:ConnectionString"]));
+
+// 3. Servicios de Seguridad e Identity
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddCookie(IdentityConstants.ApplicationScheme);
+
+builder.Services.AddIdentityCore<Usuario>()
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddApiEndpoints();
+
+// 4. OpenAPI / Scalar
 builder.Services.AddOpenApi();
 
+// 5. Inyección de dependencias de capas
 builder.Services.AddRepositoryConnector();
 builder.Services.AddServiceConnector();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 6. Pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -26,8 +44,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// 7. Middlewares de seguridad
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// 8. Endpoints de autenticación
+app.MapGroup("/api/auth")
+    .WithTags("Auth")
+    .MapIdentityApi<Usuario>();
 
 app.Run();
---
+++ ChocolatesSV.DAL/AuthDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ChocolatesSV.Entities.Models;
+
+namespace ChocolatesSV.DAL
+{
+    public class AuthDbContext : IdentityDbContext<Usuario>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+    }
+}
